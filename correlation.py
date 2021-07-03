c629d8a5d79824ed39de1e96111a07242a15f651
Nandakishor <nandakishornk01@gmail.com>
2021-07-03 13:46:45 +0530

    Initial

--- correlation.py
+++ correlation.py
@@ -1,1 +1,12 @@
 # Add the functions in this file
+def load_journal(fname):
+	f=open(fname,"r")
+	dic=[dict()];
+	cnt=0
+	for i in f:
+		dic[cnt]["events"]=i["events"]
+		cnt=cnt+1
+	f.close()
+	return dic
+
+
